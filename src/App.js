c01d9e1897d8a8daecc708b924188d035b1d5883
Kim-EunKyu <rladmsrb96@naver.com>
2021-06-11 22:24:57 +0900

    여러 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import ContentEditable from "react-contenteditable";
-import { useRef, useState } from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBold,
+  faIndent,
   faItalic,
+  faListOl,
+  faListUl,
+  faOutdent,
+  faStrikethrough,
+  faSuperscript,
   faUnderline,
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -19,7 +25,7 @@ const EditorBtns = styled.div`
   flex-direction: row;
   background-color: #f8f8f8;
   border-bottom: 1px solid #d1d1d1;
-  padding: 6px 8px 2px;
+  padding: 6px 8px 4px;
 `;
 
 const StyledContentEditable = styled(ContentEditable)`
@@ -28,21 +34,22 @@ const StyledContentEditable = styled(ContentEditable)`
 `;
 
 function App() {
+  console.log("메인 App 갱신");
   const [html, setHtml] = useState(
-    `<p>Hello <b>Worldㅇㅇ</b> !</p><p>Paragraph 2</p>`
+    `<p>Hello <b>W<i>o<u>rld</u>ㅇ</i>ㅇ</b> !</p><p>Paragraph 2</p>`
   );
-  const [optionState, setOptionState] = useState([false, false, false]);
+  const [optionState, setOptionState] = useState([false, false, false, false]);
+  const [keyList, setKeyList] = useState("initial");
+  let keysPressed = {};
 
-  const optionList = ["B", "I", "U"];
-
-  const Console = useRef();
+  const optionList = ["B", "I", "U", "STRIKE"];
 
   //Event
   const onChangeHtml = (e) => {
     setHtml(e.target.value);
   };
 
-  function selectText() {
+  const selectText = () => {
     let nodeConstruct = [];
     let selectionText = "";
     let targetNode = null;
@@ -53,65 +60,115 @@ function App() {
       selectionText = document.selection.createRange().text;
     }
     console.log(selectionText);
+    // console.log(selectionText.getRangeAt(0));
+    // console.log(selectionText.getRangeAt(0).cloneContents());
+    // const range = selectionText.getRangeAt(0);
+    // console.log(range.selectNode(range.startContainer));
 
-    targetNode = selectionText.anchorNode;
-    while (
-      targetNode.parentNode.nodeName === "U" ||
-      targetNode.parentNode.nodeName === "I" ||
-      targetNode.parentNode.nodeName === "B"
-    ) {
-      nodeConstruct.push(targetNode.parentNode.nodeName);
-      targetNode = targetNode.parentNode;
+    // console.log("텍스트 선택----------------------");
+
+    if (selectionText.anchorOffset < selectionText.focusOffset) {
+      targetNode = selectionText.anchorNode;
+      console.log("anchor", selectionText.anchorNode);
+    } else {
+      targetNode = selectionText.focusNode;
+      console.log("focus", selectionText.focusNode);
     }
 
-    let temp = new Array(3).fill(false);
-    optionList.forEach((option, index) => {
-      console.log(option, index, nodeConstruct.indexOf(option));
-      if (nodeConstruct.indexOf(option) !== -1) {
-        console.log("true", index);
-        temp[index] = true;
-      } else {
-        console.log("false", index);
-        temp[index] = false;
-      }
-    });
-    console.log("temp", temp);
-    setOptionState(1);
-    console.log(optionState);
-
-    // console.log(selectionText.anchorNode);
-    // console.log(selectionText.anchorNode.data);
-    // console.log(typeof selectionText.anchorNode.data);
-    // console.log(selectionText.anchorNode.data[selectionText.anchorOffset - 1]);
-    // console.log(selectionText.anchorNode[selectionText.anchorOffset]);
-    // const bool = window.getSelection().containsNode("<b></b>");
-    // console.log(bool);
+    console.log("전", nodeConstruct);
     console.log(targetNode);
-    console.log(nodeConstruct);
-    return optionState;
-  }
+
+    if (targetNode !== null) {
+      while (
+        targetNode.parentNode.nodeName === "U" ||
+        targetNode.parentNode.nodeName === "I" ||
+        targetNode.parentNode.nodeName === "B" ||
+        targetNode.parentNode.nodeName === "STRIKE"
+      ) {
+        nodeConstruct.push(targetNode.parentNode.nodeName);
+        targetNode = targetNode.parentNode;
+      }
+
+      console.log("전처리 전", nodeConstruct);
+      let temp = new Array(4).fill(false);
+      optionList.forEach((option, index) => {
+        if (nodeConstruct.indexOf(option) !== -1) {
+          temp[index] = true;
+        } else {
+          temp[index] = false;
+        }
+      });
+      console.log(nodeConstruct);
+      console.log("temp", temp);
+      setOptionState(temp);
+
+      return temp;
+    }
+  };
 
   const onMouseUpText = () => {
-    Console.current.innerText = selectText();
+    // Console.current.innerText = selectText();
+    selectText();
   };
 
   const onKeyUpText = (e) => {
-    Console.current.innerText = selectText();
-    console.log(e.key);
+    console.log(keyList);
+    delete keysPressed[e.key];
   };
-  // #E5E5E5
+
+  const onKeyDownKey = (e) => {
+    keysPressed[e.key] = true;
+
+    if (keysPressed["Shift"] && e.key === "Tab") {
+      e.preventDefault();
+      document.execCommand("outdent", false, null);
+    } else if (keysPressed["Tab"]) {
+      e.preventDefault();
+      document.execCommand("indent", false, null);
+    }
+  };
+
   return (
     <EditorBlock>
-      <EditorBtns>
-        <EditButton cmd="Bold">
+      <EditorBtns onClick={selectText}>
+        <EditButton cmd="bold" isClicked={optionState[0]}>
           <FontAwesomeIcon icon={faBold} />
         </EditButton>
-        <EditButton cmd="Italic">
+        <EditButton cmd="italic" isClicked={optionState[1]}>
           <FontAwesomeIcon icon={faItalic} />
         </EditButton>
-        <EditButton cmd="Underline">
+        <EditButton cmd="underline" isClicked={optionState[2]}>
           <FontAwesomeIcon icon={faUnderline} />
         </EditButton>
+        <EditButton cmd="strikeThrough" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faStrikethrough} />
+        </EditButton>
+        <Divider />
+
+        <EditButton cmd="insertOrderedList" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faListOl} />
+        </EditButton>
+        <EditButton cmd="insertUnorderedList" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faListUl} />
+        </EditButton>
+        <EditButton cmd="indent" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faIndent} />
+        </EditButton>
+        <EditButton cmd="outdent" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faOutdent} />
+        </EditButton>
+        <Divider />
+
+        <EditButton cmd="superscript" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faSuperscript} />
+        </EditButton>
+        <EditButton cmd="fontSize" isClicked={optionState[3]} arg={7}>
+          <FontAwesomeIcon icon={faSuperscript} />
+        </EditButton>
+        <EditButton cmd="superscript" isClicked={optionState[3]}>
+          <FontAwesomeIcon icon={faSuperscript} />
+        </EditButton>
+        <button onClick={() => alert(keyList)}>확인</button>
       </EditorBtns>
 
       <StyledContentEditable
@@ -119,13 +176,18 @@ function App() {
         onChange={onChangeHtml}
         onMouseUp={onMouseUpText}
         onKeyUp={onKeyUpText}
+        onKeyDown={onKeyDownKey}
       />
-      <div ref={Console}></div>
     </EditorBlock>
   );
 }
 
-function EditButton({ cmd, name, arg, children }) {
+const EditButton = React.memo(function EditButton({
+  cmd,
+  isClicked,
+  arg,
+  children,
+}) {
   return (
     <EditBtn
       key={cmd}
@@ -133,21 +195,42 @@ function EditButton({ cmd, name, arg, children }) {
         evt.preventDefault(); // Avoids loosing focus from the editable area
         document.execCommand(cmd, false, arg); // Send the command to the browser
       }}
+      isClicked={isClicked}
     >
       {children}
     </EditBtn>
   );
-}
+});
 
 const EditBtn = styled.button`
-  background-color: #f8f8f8;
-  border: none;
+  /* display: flex; */
+  /* flex-direction: row; */
+  /* justify-content: center; */
+  /* align-items: center; */
   outline: none;
   cursor: pointer;
 
   &:hover {
     background-color: #e5e5e5;
+    border: 1px solid #bcbcbc;
   }
+
+  ${(props) =>
+    props.isClicked
+      ? css`
+          background-color: #ffffff;
+          border: 1px solid #bcbcbc;
+        `
+      : css`
+          background-color: #f8f8f8;
+          border: 1px solid #f8f8f8;
+        `}
+`;
+
+const Divider = styled.div`
+  width: 1px;
+  background-color: #d1d1d1;
+  margin: 0 2px;
 `;
 
 export default App;
